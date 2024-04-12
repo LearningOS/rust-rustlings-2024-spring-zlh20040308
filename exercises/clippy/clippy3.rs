78c82fbc8fa9fd717bb3dcf27ad762e8583a01d4
Feng <2165926681@qq.com>
2024-04-12 15:33:04 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -4,12 +4,13 @@
 //
 // Execute `rustlings hint clippy3` or use the `hint` watch subcommand for a hint.
 
+// I AM NOT DONE
 
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        panic!("aaaa");
+        println!("It is {:#?}", my_option);
     }
 
     let my_arr = &[
@@ -18,7 +19,8 @@ fn main() {
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
-    let my_empty_vec: std::vec::Vec<i32> = vec![];
+    let mut my_empty_vec = vec![1, 2, 3, 4, 5];
+    my_empty_vec.clear();
     println!("This Vec is empty, see? {:?}", my_empty_vec);
 
     let mut value_a = 45;
